db4fb4b74841edbb132a21a9261f38fed577fd5b
Martin Cox <coxmartin877@gmail.com>
2025-11-25 01:07:25 -0600

    Se implemento opcion para eliminar productos

--- Routes/principal/principal.js
+++ Routes/principal/principal.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const { verificacionSesion } = require('../global')
 const principal = express.Router()
-const {obtenerProductos} = require('../../db_conexion')
+const {obtenerProductos, eliminarRegistros} = require('../../db_conexion')
 
 principal.use(verificacionSesion);
 
-principal.get('/', async (request, response) => {
+principal.get('/', verificacionSesion, async (request, response) => {
     const usuario = request.usuario;
     if (!usuario) return response.status(403).json({ acceso: false });  // Si no hay usuario, no hay acceso ni data.
 
@@ -13,6 +13,17 @@ principal.get('/', async (request, response) => {
     return response.status(200).json({ acceso: true, informacion: productos })
 })
 
+principal.delete('/', verificacionSesion, async (request, response) => {
+    const usuario = request.usuario;
+    if (!usuario) return response.status(403).json({ acceso: false });  // Si no hay usuario, no hay acceso ni data.
+
+    const { id } = request.body;
+    if (!id || id.length > 10) return response.status(400).json({ acceso: false });
+    
+    const producto = await eliminarRegistros(id);
+    if (!producto) return response.status(404).json({ acceso: false });
+    response.status(200).json({ acceso: true, informacion: producto })
+})
 module.exports = {
     principal
 }
--- db_conexion.js
+++ db_conexion.js
@@ -2,10 +2,10 @@ const { createClient } = require('@supabase/supabase-js')
 
 const cliente = createClient('https://ouvrbcxtbmsthupeuzyk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91dnJiY3h0Ym1zdGh1cGV1enlrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MzQyNjM1OSwiZXhwIjoyMDc5MDAyMzU5fQ.Y1l0480GLSrNwm-RBNrIC7DvZkIgLdUfYc6Z2AFsy14')
 
-const buscarUsuario = async(usuario) => {
+const buscarUsuario = async (usuario) => {
     try {
         const { data } = await cliente.from('Usuario').select('nombre_usuario, contrasena, id').eq('nombre_usuario', usuario).range(0, 1)
-        if(data.length > 0) return data
+        if (data.length > 0) return data
         return false
     } catch {
         return false
@@ -15,15 +15,26 @@ const buscarUsuario = async(usuario) => {
 // Obtiene todos los productos.
 const obtenerProductos = async () => {
     try {
-        const { data } = await cliente.from('Producto').select('*');
-        if(data.length > 0) return data
+        const { data } = await cliente.from('Producto').select('*')
+        if (data.length > 0) return data
         return false
-    }catch {
+    } catch {
+        return false
+    }
+}
+
+const eliminarRegistros = async (ids) => {
+    try {
+        const { data } = await cliente.from('Producto').delete().in('id', ids).select()
+        if (data.length > 0) return data
+        return false
+    } catch {
         return false
     }
 }
 
 module.exports = {
     buscarUsuario,
-    obtenerProductos
+    obtenerProductos,
+    eliminarRegistros
 }
