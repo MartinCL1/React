486bdf5e87700d474e0f1b6934958e131c475777
Martin Cox <coxmartin877@gmail.com>
2025-11-27 17:07:25 -0600

    cambio de ruta

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.use('/principal', principal);
 app.get('/', (req, res) => {
     res.send('Hola Mundo')
 })
-
 app.listen(PORT, ()=> {
     console.log('El server esta corriendo en el puerto: ', PORT)
 })
