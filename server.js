9fa8dc29aa9812793a13f333c1f7fc58da4ef3af
Martin Cox <coxmartin877@gmail.com>
2025-11-27 17:08:44 -0600

    cambio de server

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const cors = require('cors')
 const login = require('./Routes/autenticacion/login')
 const { principal } = require('./Routes/principal/principal')
 const app = express()
-const PORT = 3500
+const PORT = 3500 || process.env.PORT
 
 app.use(cookieParser())
 app.use(cors({credentials: true, origin: ['http://localhost:5173', 'https://panaben.netlify.app']}))
 app.use(express.json())
 
 // Rutas
+
+app.get('/', (req, res) => {
+    res.send('Hola Mundo')
+})
+
 app.use('/login', login );
 app.use('/principal', principal);
 
