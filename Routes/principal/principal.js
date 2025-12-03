41ee8eeb13094d873e6c29988f9f56e8733f1dfd
Martin Cox <coxmartin877@gmail.com>
2025-12-02 21:56:15 -0600

    nueva data

--- Routes/principal/principal.js
+++ Routes/principal/principal.js
@@ -1,48 +1,37 @@
 const express = require('express')
 const { verificacionSesion } = require('../global')
-const { obtenerProductos, eliminarRegistros, agregarProducto, editarProducto, paginarDatos } = require('../../db_conexion')
+const {obtenerProductos, eliminarRegistros, agregarProducto, editarProducto} = require('../../db_conexion')
 const principal = express.Router()
 
 principal.use(verificacionSesion);
 
-
-principal.delete('/', verificacionSesion, async (request, response) => {
+principal.get('/', verificacionSesion, async (request, response) => {
     const usuario = request.usuario;
     if (!usuario) return response.status(403).json({ acceso: false });
 
+    const productos = await obtenerProductos();  
+    return response.status(200).json({ acceso: true, informacion: productos })
+})
+
+principal.delete('/', verificacionSesion, async (request, response) => {
+    const usuario = request.usuario;
+    if (!usuario) return response.status(403).json({ acceso: false });  
+
     const { id } = request.body;
     if (!id || id.length > 10) return response.status(400).json({ acceso: false });
-
+    
     const producto = await eliminarRegistros(id);
     if (!producto) return response.status(404).json({ acceso: false });
     response.status(200).json({ acceso: true, informacion: producto })
 })
 
-principal.put('/actualizarProducto', async (request, response) => {
+principal.put('/actualizarProducto', async(request, response) => {
     const usuario = request.usuario;
-<<<<<<< HEAD
     if(!usuario) return response.status(403).json({acceso: false});
     const producto = request.body
-=======
-    if (!usuario) return response.status(403).json({ acceso: false });
-    const producto = request.body
-    const expresion = /^[A-Za-z ]+$/
-    const expresionEnteros = /^[0-9]+$/
-    const expresionPrecios = /^[0-9]+(\.[0-9]+)?$/
-
-    if (!producto.nombre.match(expresion) ||
-        producto.existente.match(expresionEnteros) ||
-        producto.actual.match(expresionEnteros) ||
-        producto.vendido.match(expresionEnteros) ||
-        producto.precio_unidad.match(expresionPrecios)
-    ) {
-        return response.status(401).json({ acceso: false })
-    }
-
->>>>>>> desarrollo
     const valor = await editarProducto(producto);
-    if (!valor) return response.status(403).json({ acceso: false });
-    response.status(200).json({ acceso: true, informacion: valor })
+    if(!valor) return response.status(403).json({acceso: false});
+    response.status(200).json({acceso: true, informacion: valor})
 })
 
 principal.post('/agregarProducto', verificacionSesion, async (request, response) => {
@@ -51,19 +40,10 @@ principal.post('/agregarProducto', verificacionSesion, async (request, response)
 
     const { id, nombre, precio_unidad, existente, actual, vendido } = request.body;
     if (!id || !nombre || !precio_unidad || !existente || !actual || !vendido) return response.status(400).json({ acceso: false });
-
-    const producto = await agregarProducto({ id, nombre, precio_unidad, existente, actual, vendido });
+    
+    const producto = await agregarProducto({id, nombre, precio_unidad, existente, actual, vendido});
     if (!producto) return response.status(404).json({ acceso: false });
     response.status(200).json({ acceso: true, informacion: producto })
-})
-
-principal.get('/obtenerData/:pagina', async (request, response) => {
-    const pagina = request.params.pagina
-    const idInicial = (pagina * 10) - 10
-    const idFinal = (pagina * 10);
-    const informacion = await paginarDatos(idInicial, idFinal)
-    if (!informacion) return response.status(401).json({ acceso: false });
-    response.status(200).json({ acceso: true, informacion: informacion })
 })
 
 module.exports = {
